0bdb2d61600f20198340129bbecd5912d2c2ddac
to236663 <tommyfernandez099@gmail.com>
2026-03-09 23:16:00 -0400

    Created React Project and Basic Landing Page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Collection Tracker</h1>
+        <p className="App-description">
+          This tracker will be to for helping collectors be able to track
+          they're own personal collections and the items they have within these
+          collections. Users will be able to create collections based on what it
+          is they are trying to track and add, edit, and delete items within
+          these collection lists as they continue to expand on their
+          collections, making this the perfect tool for collectors to manage and
+          keep organize their hobby of collecting!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
